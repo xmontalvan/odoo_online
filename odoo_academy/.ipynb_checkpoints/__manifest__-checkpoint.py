be6cb970fe8e1331642d7ea104f33292f52fb0cd
xmontalvan <xiomara.montalvan.m@gmail.com>
2021-11-24 03:37:59 +0000

    (ADD) Basic Module

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy  app to manage Training """
+    'description':"""
+        Academy Module to manage Training
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    'author': 'Odoo',
+    'website': '',
+    'category': 'Trainning',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        
+    ],
+    'demo': [
+        
+    ],
+}
